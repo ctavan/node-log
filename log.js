51482fe2f43257ad2265531c5c1e5aa7d686911b
Jonas Dohse <jonas@mbr-targeting.com>
2017-08-04 12:57:17 +0200

    Migrate to ES6

--- log.js
+++ log.js
@@ -1,43 +1,37 @@
 // Copyright 2011-2012 mbr targeting GmbH. All Rights Reserved.
 
-var util = require('util');
+const util = require('util');
 
-var getenv = require('getenv');
+const getenv = require('getenv');
 
-var logLevels = ['fatal', 'error', 'warn', 'info', 'debug'];
+const logLevels = ['fatal', 'error', 'warn', 'info', 'debug'];
 
 function findErrors(args) {
-  return args.filter(function(arg) {
-    return arg instanceof Error;
-  });
+  return args.filter((arg) => arg instanceof Error);
 }
 
-var logStack = getenv.bool('LOG_STACK', true);
+const logStack = getenv.bool('LOG_STACK', true);
 function formatErrors(errors) {
   if (!logStack) {
     return '';
   }
 
-  var output = [''];
-  errors.forEach(function(error) {
+  const output = [''];
+  errors.forEach((error) => {
     output.push(error.stack);
 
-    var object = util.inspect(error, true, 3);
+    const object = util.inspect(error, true, 3);
     output.push(object);
   });
   return output.join('\n');
 }
 
 function forward(target, prefix) {
-  return function(format) {
-    format = prefix + format;
-
-    var args = Array.prototype.slice.call(arguments);
-
-    var errors = findErrors(args);
+  return (format, ...args) => {
+    const errors = findErrors(args);
 
     console[target]('%s%s',
-                    util.format.apply(null, args),
+                    util.format(prefix + format, ...args),
                     formatErrors(errors));
   };
 }
@@ -48,8 +42,8 @@ exports.warn = forward('warn', 'WARN : ');
 exports.info = forward('log', 'INFO : ');
 exports.debug = forward('log', 'DEBUG: ');
 
-var logLevel = getenv.string('LOG_LEVEL', 'debug');
-var index = logLevels.indexOf(logLevel);
+const logLevel = getenv.string('LOG_LEVEL', 'debug');
+const index = logLevels.indexOf(logLevel);
 if (index === -1) {
   throw new Error('Log.InvalidLogLevel: ' + logLevel + ', set LOG_LEVEL');
 }
